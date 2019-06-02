13301086978bf3ca1d3d8caaccdc2e833a3abe40
Matheus <Binotto>
2019-06-02 16:03:34 -0300

    Serving up Static Assets - Help

--- src/app.js
+++ src/app.js
@@ -7,17 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath))
 
-//app.com/help
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-//app.com/about
-app.get('/about', (req, res) => {
-    res.send({
-        name: 'Matheus Binotto'
-    })
-})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
