be2a682d8ff23a78e70eb902cab7dfd47cb941e0
Matheus <Binotto>
2019-06-02 18:40:59 -0300

    Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,37 +1,41 @@
 const path = require('path')
 const express = require('express')
-const express = require('hbs')
-
+const hbs = require('hbs')
 
 const app = express()
 
-//Define paths for Express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-//Setup handlebars engine and views location
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-//Setup static directory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Full Stack Web Developer',
-        name:  'Matheus Binotto'
+        name:  'Matheus Binotto',
+        site: 'My site'
     })
 })
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'Full Stack Web Developer',
-        name:  'Matheus Binotto'
+        name:  'Matheus Binotto',
+        site: 'My site - About me'
     })
 })
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Full Stack Web Developer',
-        name:  'Matheus Binotto'
+        name:  'Matheus Binotto',
+        site: 'My site - Help'
     })
 })
 
