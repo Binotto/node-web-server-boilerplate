d0c08c3500201dec30d750a451d90eba3ea90721
Matheus <Binotto>
2019-06-02 17:25:53 -0300

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -3,12 +3,31 @@ const express = require('express')
 
 
 const app = express()
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.set('view engine', 'hbs')
-
-
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Full Stack Web Developer',
+        name:  'Matheus Binotto'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Full Stack Web Developer',
+        name:  'Matheus Binotto'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Full Stack Web Developer',
+        name:  'Matheus Binotto'
+    })
+})
+
 
 
 app.listen(3000, () => {
